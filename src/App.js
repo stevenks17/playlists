046cd2db2847fbc233a0bd97c1a5ecfab33d9fe9
stevenks17 <stevenks17@gmail.com>
2020-08-11 19:37:13 -0400

    login component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="app">
-      <h1> Test Page </h1>
+
+      {/* Login with spotify button */}
+
+      <Login />
+
     </div>
   );
 }
