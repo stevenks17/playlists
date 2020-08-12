ad34605039aa0d189dcc7a9887491029601bdd9a
stevenks17 <stevenks17@gmail.com>
2020-08-12 16:56:56 -0400

    fixed and added custom logo

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Login.css';
 
-
 function Login() {
     return (
         <div className="login">       
-
-
+            <img
+                src={require('/mnt/c/Development/Projects/playlists/src/Playlists.png')}
+                alt=""
+            />
             <button> Login with your spotify account </button>
         </div>
     )
