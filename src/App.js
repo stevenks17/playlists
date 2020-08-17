e1e27fd26a9c182a2d67094de3815e7b86bd5c27
stevenks17 <stevenks17@gmail.com>
2020-08-17 14:42:18 -0400

    imported StateProviderValue and created dispatch to context store.

--- src/App.js
+++ src/App.js
@@ -4,30 +4,39 @@ import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from "spotify-web-api-js"
 import Player from './Player'
+import { useStateProviderValue} from './StateProvider'
+
 const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState(null)
+  const [{ user }, dispatch] = useStateProviderValue()
 
  // run this on condition specified below
   useEffect(() => {
     const hash = getTokenFromUrl()
     window.location.hash = ""
-    const _token = hash.acces_token
+    let _token = hash.acces_token
 
     if (_token) {
       setToken(_token)
+
       
       spotify.setAccessToken(_token)
-
+      
       spotify.getMe().then(user => {
-        console.log(user)
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
       })
     }
 
     console.log(" THIS IS MY TOKEN -->", token)
     // Condition  
   }, [])
+
+  console.log(user)
 
   return (
     <div className="app">{token ? <Player /> : <Login />}</div>
