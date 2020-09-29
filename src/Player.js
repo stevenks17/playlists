321860c939a71ed465be0ac46343497558c806d6
stevenks17 <stevenks17@gmail.com>
2020-09-29 12:15:11 -0400

    Player CSS generated/ Body + Sidebar components added

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./Player.css"
+import Sidebar from './Sidebar'
+import Body from './Body'
 
-function Player() {
+function Player({ spotify }) {
     return (
-        <div>
-            <h1> Welcome to Playlists</h1>
+        <div className= "player">
+            <div className="player__body">
+            <Sidebar />
+            <Body />               
+            </div>
+
+            {/*Footer*/}
+
         </div>
     )
 }
