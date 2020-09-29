766276f9a3dfa1525dfb8477da526583e2526507
stevenks17 <stevenks17@gmail.com>
2020-09-29 16:22:37 -0400

    Sidebar image and styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
     return (
         <div>
-            
+            <img src="/mnt/c/Development/Projects/playlists/src/v3.png" alt="" />
         </div>
     )
 }
